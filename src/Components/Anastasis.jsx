44f5480635db2b102521e98b5b201f6f92100927
Anastasis Maltsios <anastashsm1234@gmail.com>
2025-01-16 15:57:57 +0200

    Fixed anchor button

--- src/Components/Anastasis.jsx
+++ src/Components/Anastasis.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../styles/Anastasis.css";
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 function Anastasis() {
     return (
@@ -7,7 +8,7 @@ function Anastasis() {
         <div id="home" className="container">
             <h1><span>I'm Anastasis Maltsios,</span> fullstack Developer based in Greece</h1>
             <p>I'm a deveolper from Serres, with projects based both on backend and frontend. </p>
-            <div className="anas-button">Contact</div>
+            <div> <AnchorLink offset={() => -20} href="#contact" className="anas-button">Contact</AnchorLink></div>
         </div>
     )
 }
